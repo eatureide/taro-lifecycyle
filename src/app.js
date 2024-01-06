eead5110ad7e2f7015742e854ca5034fa20b1320
ZeJun.Yi <zejun.yi@watsons.com.cn>
2024-01-06 18:44:01 +0800

    1

---
+++ src/app.js
@@ -0,0 +1,22 @@
+import { Component } from 'react'
+import './app.scss'
+
+class App extends Component {
+
+  globalData = {
+    code: ''
+  }
+
+  async componentWillMount() {
+    const { code } = await wx.login()
+    this.globalData.code = code
+  }
+
+  render() {
+    return (
+      this.props.children
+    )
+  }
+}
+
+export default App
